8c61c44be08c32fdd19e18362e25aca8d47eb468
Manish Yadav2 <manish.yadav2@mavenir.com>
2020-01-09 16:59:13 -0800

    InitialSetup

---
+++ MyFramework/MyFramework/MyFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyFramework.h
+//  MyFramework
+//
+//  Created by Manish Yadav on 1/9/20.
+//  Copyright © 2020 Manish Yadav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyFramework.
+FOUNDATION_EXPORT double MyFrameworkVersionNumber;
+
+//! Project version string for MyFramework.
+FOUNDATION_EXPORT const unsigned char MyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFramework/PublicHeader.h>
+
+
---
+++ MyFramework/MyFramework/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  MyFramework
+//
+//  Created by Manish Yadav on 1/9/20.
+//  Copyright © 2020 Manish Yadav. All rights reserved.
+//
+
+import Foundation
+public class Service{
+    private init(){}
+    
+    public static func doSomething() -> String{
+        return "Hey Hey"
+    }
+}
